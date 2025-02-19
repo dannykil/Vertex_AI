881d44f06f353015b8abe8a26d258e94fb90d187
danniel.kil <danniel.kil@ip-10-23-9-177.ap-northeast-2.compute.internal>
2025-02-19 15:54:22 +0900

    fixing test_api

--- test_api.py
+++ test_api.py
@@ -131,13 +131,13 @@ def example_get():
     # logger.info("Application started.")  # INFO 레벨 로그
     # print("This message will also be logged.")  # print() 문으로 출력된 메시지도 로그에 포함 가능
 
-    try:
-        result = 10 / 0
-    except ZeroDivisionError as e:
-        logger.exception("An error occurred: %s", e)  # 에러 발생 시 traceback과 함께 로그 기록
+    # try:
+    #     result = 10 / 0
+    # except ZeroDivisionError as e:
+    #     logger.exception("An error occurred: %s", e)  # 에러 발생 시 traceback과 함께 로그 기록
 
-    logger.warning("Application is running low on resources.")  # WARNING 레벨 로그
-    logger.critical("A critical error occurred. Shutting down.")  # CRITICAL 레벨 로그
+    # logger.warning("Application is running low on resources.")  # WARNING 레벨 로그
+    # logger.critical("A critical error occurred. Shutting down.")  # CRITICAL 레벨 로그
 
 
     return host + "_" + now.strftime('%Y-%m-%d %H:%M:%S') + "_v9"
